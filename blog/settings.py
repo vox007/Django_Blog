7db7ae642999b61507c3e6a2312f508131a6bb64
unknown <gtdragon382@gmail.com>
2019-10-02 23:02:42 +0700

    Added Django

--- blog/settings.py
+++ blog/settings.py
@@ -1,6 +1,7 @@
 
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -167,3 +168,5 @@ AUTHENTICATION_BACKENDS = (
 
 
 SITE_ID = 1
+
+django_heroku.settings(locals())
